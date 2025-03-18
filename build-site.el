d1f1e6bbec543b427e00c3fd8a19d08a07e8ade2
ANAND S <135801803+anandsundaramoorthysa@users.noreply.github.com>
2025-03-18 21:16:41 +0530

    Update build-site.el

--- build-site.el
+++ build-site.el
@@ -1,5 +1,4 @@
-;; Set the package installation directory so that packages aren't stored in the
-;; ~/.emacs.d/elpa path.
+;; Set package installation directory
 (require 'package)
 (setq package-user-dir (expand-file-name "./.packages"))
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -30,44 +29,51 @@
 
 (defun goldayan/head-extra ()
   (concat
+   "<link rel=\"shortcut icon\" href=\"/assets/logo.jpg\" type=\"image/jpeg\">"
    "<link href=\"https://fonts.cdnfonts.com/css/share-tech-mono\" rel=\"stylesheet\">"
    "<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/code.css\"/>"
-   "<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/style.css\"/>"))
+   "<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/style.css\"/>")) ;; Updated for CSS-only menu
 
-;; Website template
+;; Define the HTML template
 (defun org-html-template (contents info)
   "Custom HTML template for org-publish"
   (concat "<!DOCTYPE html>"
-"<html>"
+"<html lang=\"en\">"
   "<head>"
+    "<meta charset=\"UTF-8\">"
+    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+    "<title>Gold Ayan's Tinker Garage</title>"
     (concat "<!-- " (org-export-data (org-export-get-date info "%Y-%m-%d") info) " -->")
     (org-html--build-meta-info info)
     (org-html--build-head info)
   "</head>"
   "<body>"
+
     "<header>"
       "<div class=\"container\">"
-	"<h1>Gold Ayan's Tinker Garage</h1>"
+        "<h1>Gold Ayan's Tinker Garage</h1>"
       "</div>"
       "<div class=\"header-content\">"
-	"<nav class=\"container\">"
-	  "<ul class=\"topnav\">"
-	    "<li><a href=\"/index.html\">Home</a></li>"
-	    "<li><a href=\"/projects.html\">Projects</a></li>"
-	    "<li><a href=\"/posts\">Posts</a></li>"
-	    "<li><a href=\"/til.html\">TIL</a></li>"
-	    "<li><a href=\"/blogrolls.html\">Blogrolls</a></li>"
-	    "<li><a href=\"/feed.xml\">RSS</a></li>"
-	  "</ul>"
-	"</nav>"
+        "<nav class=\"container\">"
+          "<input type=\"checkbox\" id=\"menu-toggle\">"
+          "<label for=\"menu-toggle\" class=\"hamburger\">☰</label>"
+          "<ul class=\"topnav\">"
+            "<li><a href=\"/index.html\">Home</a></li>"
+            "<li><a href=\"/projects.html\">Projects</a></li>"
+            "<li><a href=\"/posts\">Posts</a></li>"
+            "<li><a href=\"/til.html\">TIL</a></li>"
+            "<li><a href=\"/blogrolls.html\">Blogrolls</a></li>"
+            "<li><a href=\"/feed.xml\">RSS</a></li>"
+          "</ul>"
+        "</nav>"
       "</div>"
     "</header>"
 
-    "<article class=\"container\">"
-      "<div class=\"blog-content\">"
+    "<main class=\"container\">"
+      "<article class=\"blog-content\">"
       contents
-      "</div>"
-    "</article>"
+      "</article>"
+    "</main>"
 
     "<footer>"
       "This site is made with ❤️ using Emacs."
@@ -78,13 +84,10 @@
 ))
 
 ;; Customize the HTML output
-(setq org-html-validation-link nil            ;; Don't show validation link
-      org-html-head-include-scripts nil       ;; Use our own scripts
-      org-html-head-include-default-style nil ;; Use our own styles
-      org-html-htmlize-output-type 'css
-      ;;org-publish-use-timestamps-flag t
-      ;;org-publish-timestamp-directory "./.org-cache/"
-      )
+(setq org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+      org-html-htmlize-output-type 'css)
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -92,22 +95,22 @@
        (list "goldayan:main"
              :recursive t
              :base-directory "./content"
-	     :base-extension "org"
-	     :html-head-extra (goldayan/head-extra)
+             :base-extension "org"
+             :html-head-extra (goldayan/head-extra)
              :publishing-function 'org-html-publish-to-html
              :publishing-directory "./public"
-	     :htmlized-source t
-             :with-author nil           ;; Don't include author name
-             :with-creator nil          ;; Include Emacs and Org versions in footer
-             :with-toc nil              ;; Include a table of contents
-             :section-numbers nil       ;; Don't include section numbers
-             :time-stamp-file nil)      ;; Don't include time stamp in file
-       (list "goldayan:assets"          ;; Copy all the assets - images, css and js
+             :htmlized-source t
+             :with-author nil
+             :with-creator nil
+             :with-toc nil
+             :section-numbers nil
+             :time-stamp-file nil)
+       (list "goldayan:assets"
              :recursive t
              :base-directory "./assets"
-	     :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|html\\|mp4\\|ico"
+             :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|html\\|mp4\\|ico"
              :publishing-function 'org-publish-attachment
-             :publishing-directory "./public/assets")))    
+             :publishing-directory "./public/assets")))
 
 ;; Generate the site output
 (org-publish-all t)
@@ -115,14 +118,13 @@
 (message "Build complete!")
 
 ;; RSS builder
-
 (webfeeder-build "./feed.xml"
-                   "./public"
-                   "https://goldayan.in"
-                   (delete "posts/index.html" (let ((default-directory (expand-file-name "./public")))
-                     (directory-files-recursively "posts" ".*\\.html$")))
-                   :builder 'webfeeder-make-rss
-                   :title "Gold Ayan's Tinker Garage"
-                   :description "Let’s tinker!"
-                   :author "Gold Ayan")
-
+                 "./public"
+                 "https://goldayan.in"
+                 (delete "posts/index.html"
+                         (let ((default-directory (expand-file-name "./public")))
+                           (directory-files-recursively "posts" ".*\\.html$")))
+                 :builder 'webfeeder-make-rss
+                 :title "Gold Ayan's Tinker Garage"
+                 :description "Let’s tinker!"
+                 :author "Gold Ayan")
